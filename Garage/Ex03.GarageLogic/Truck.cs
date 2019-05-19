5c724ccff298f28fbb69cedf8ba46d54e09fcd74
Ido Hayun <eidoil32@gmail.com>
2019-05-19 10:57:10 +0300

    Update files

---
+++ Garage/Ex03.GarageLogic/Truck.cs
@@ -0,0 +1,25 @@
+﻿namespace n_Truck
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using n_Vehicle;
+    using n_Wheel;
+
+    public class Truck : Vehicle
+    {
+        public static readonly int sr_NumberOfWheels = 12, sr_FullTunkLevel = 110, sr_MaxPressure = 26;
+        public static readonly eEnergyType sr_EnergyType = eEnergyType.Soler;
+        bool m_HazardousMaterials;
+        readonly float r_TrunkLevel;
+
+        public Truck(string i_ModelName, string i_PlateNumber, bool v_isDeliveryHazardousMaterials, float i_TrunkLevel) 
+            : base(sr_NumberOfWheels, sr_FullTunkLevel, i_ModelName,i_PlateNumber)
+        {
+            m_HazardousMaterials = v_isDeliveryHazardousMaterials;
+            r_TrunkLevel = i_TrunkLevel;
+        }
+    }
+}
---
+++ Garage/Ex03.GarageLogic/Wheel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace n_Wheel
+{
+    public class Wheel
+    {
+        string m_Manufacturer;
+        float m_CurrentPressure;
+        readonly float r_MaxPressure;
+
+        public Wheel(string i_ManufacturerName, float i_MaxPressure)
+        {
+            m_Manufacturer = i_ManufacturerName;
+            m_CurrentPressure = i_MaxPressure;
+            r_MaxPressure = i_MaxPressure;
+        }
+
+        public void FillTyre(float i_UnitToFill)
+        {
+            if(m_CurrentPressure + i_UnitToFill > r_MaxPressure)
+            {
+                m_CurrentPressure = r_MaxPressure;
+            }
+            else
+            {
+                m_CurrentPressure += r_MaxPressure;
+            }
+        }
+
+        public float CurrentPressure
+        {
+            get { return m_CurrentPressure; }
+            set { m_CurrentPressure = value; }
+        }
+
+        public float MaxPressure
+        {
+            get { return r_MaxPressure; }
+        }
+
+        public string ManufacturerName
+        {
+            get { return m_Manufacturer; }
+            set { m_Manufacturer = value; }
+        }
+    }
+}
